28500f5c8d589dc481bbd143d564ca41dccb020a
CurtisNewbie <zhuangyongj@gmail.com>
2021-08-12 13:57:05 +0800

    Support fetching list selectively

--- distributed-task-module/src/main/java/com/curtisnewbie/module/task/dao/TaskMapper.java
+++ distributed-task-module/src/main/java/com/curtisnewbie/module/task/dao/TaskMapper.java
@@ -11,17 +11,43 @@ import java.util.List;
  */
 public interface TaskMapper {
 
+    /**
+     * Insert
+     */
     int insert(TaskEntity record);
 
+    /**
+     * Select * by id
+     */
     TaskEntity selectByPrimaryKey(Integer id);
 
+    /**
+     * Select *
+     */
     List<TaskEntity> selectAll();
 
+    /**
+     * Update last_run_start_time, last_run_end_time, last_run_result, last_run_by by id
+     */
     void updateLastRunInfo(TaskEntity te);
 
+    /**
+     * Select 1 by id
+     */
     Integer findOneById(@Param("id") int taskId);
 
+    /**
+     * Update enabled by id
+     */
     void updateEnabled(@Param("id") int taskId, @Param("enabled") int enabled);
 
+    /**
+     * Update enabled, result by id
+     */
     void updateEnabledAndResult(@Param("id") int taskId, @Param("enabled") int enabled, @Param("result") String result);
+
+    /**
+     * Select * by job_name, enabled, app_group
+     */
+    List<TaskEntity> selectBy(TaskEntity param);
 }
